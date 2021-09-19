86e51dd1239efe95782e6e864082665562aa5a45
Dreaprince <toheebodus@gmail.com>
2021-09-19 20:58:42 +0100

    second commit

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ require('./startup/route')(app);
 require('./startup/db')();
 require('./startup/config')();
 require('./startup/validation')();
+require('./startup/prod')(app);
 
 
 const port = process.env.PORT || 3021;
